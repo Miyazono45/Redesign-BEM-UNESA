ec9b927773deee676fccf6d2693404d32b73e926
rskhxzz <muhamadfililmi.22076@mhs.unesa.ac.id>
2023-10-20 00:28:34 +0700

    final

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import React, { useRef, useEffect, useState } from "react";
 import Logo from "../../../public/logo-unesa.png";
@@ -14,7 +13,7 @@ const Footer = () => {
   const handleScroll = () => {
     const element = elementRef.current;
     const elementPosition = element.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight - 100; // offset 100px dari atas jendela
+    const windowHeight = window.innerHeight - 100;
 
     if (elementPosition < windowHeight) {
       setIsVisible(true);
@@ -34,18 +33,22 @@ const Footer = () => {
     <div
       ref={elementRef}
       className={`bg-white mt-10 animate-fade-up ${
-        isVisible ? "is-visible" : ""
+        isVisible ? "is-visible is-entering" : ""
       }`}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        transform: isVisible ? "translateY(0)" : "translateY(40%)",
+        transition: "opacity 0.8s ease-in, transform 0.8s ease-in",
+      }}
     >
       <div className="flex flex-row">
         <div className="bottom-0">
-          {/* <div className="border-2  bg-[url('/twa.svg')] bg-cover"> */}
           <Image
             src={Tes}
-            className=" absolute mt-[84px]  h-[26rem] w-[110rem] z-10 -ml-6 "
+            className="absolute mt-[84px] h-[26rem] w-[110rem] z-10 -ml-6"
             alt=""
           />
-          <div className=" ">
+          <div className="">
             <Image
               src={Twa}
               className="absolute z-20 w-[95rem] h-[31rem] -ml-6"
@@ -53,42 +56,31 @@ const Footer = () => {
             />
           </div>
           <div className="flex flex-row">
-            <div className="mr-[200px] mt-[160px] ml-[130px] flex flex-col z-30 ">
+            <div className="mr-[200px] mt-[160px] ml-[130px] flex flex-col z-30">
               <p className="font-extrabold text-[3.1em] text-[#F2F2F2]">
                 <a className="text-[#1F183C]">BEM</a> UNIVERSITAS
               </p>
-              <p className=" mt-[-10px] font-extrabold text-[3.1em] text-[#F0EFF1]">
+              <p className="mt-[-10px] font-extrabold text-[3.1em] text-[#F0EFF1]">
                 NEGERI <a className="text-[#1F183C]">SURABAYA</a>
               </p>
-              <div className="flex flex-row" >
+              <div className="flex flex-row">
                 <Image src={Logo} className="w-[105px] h-[130px] mt-2" alt="" />
-                <Image src={BEM} className=" ml-5 w-[65px] h-[150px]" alt="" />
+                <Image src={BEM} className="ml-5 w-[65px] h-[150px]" alt="" />
                 <Image
                   src={Karya}
-                  className=" mt-10 ml-5 w-[155px] h-[60px]"
+                  className="mt-10 ml-5 w-[155px] h-[60px]"
                   alt=""
                 />
               </div>
             </div>
-            <div className=" mt-[245px]  text-gray-800 flex flex-col z-40 ml-[125px]">
-              <p className="text-[#1F183C] font-extrabold text-[1.5em]">
-                Email:
-              </p>
+            <div className="mt-[245px] text-gray-800 flex flex-col z-40 ml-[125px]">
+              <p className="text-[#1F183C] font-extrabold text-[1.5em]">Email:</p>
               <p className="font-medium -mt-2 text-[1.0em]">bem@unesa.ac.id</p>
-              <p className="text-[#1F183C] font-extrabold text-[1.5em]">
-                Hubungi Kami:
-              </p>
-              <p className="font-medium -mt-2 text-[1.0em]">
-                Feby : +6281254155827
-              </p>
-              <p className="text-[#1F183C] font-extrabold text-[1.5em]">
-                Seketariat BEM UNESA:
-              </p>
-              <p className="font-medium -mt-2 text-[1.0em]">
-                Jl. Unesa, Kota Surabaya
-              </p>
+              <p className="text-[#1F183C] font-extrabold text-[1.5em]">Hubungi Kami:</p>
+              <p className="font-medium -mt-2 text-[1.0em]">Feby : +6281254155827</p>
+              <p className="text-[#1F183C] font-extrabold text-[1.5em]">Seketariat BEM UNESA:</p>
+              <p className="font-medium -mt-2 text-[1.0em]">Jl. Unesa, Kota Surabaya</p>
             </div>
-            {/* </div> */}
           </div>
         </div>
       </div>
