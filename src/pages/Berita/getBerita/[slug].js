b916fe2198643d926399d1d08f3b7f1e54a327a9
Adam <withdaamleon@gmail.com>
2023-10-19 23:06:24 +0700

    Update [slug].js

--- src/pages/Berita/getBerita/[slug].js
+++ src/pages/Berita/getBerita/[slug].js
@@ -1,113 +1,56 @@
-'use client'
+"use client";
 
+import { useRouter } from "next/router";
 
-import { useRouter } from 'next/router'
+import Image from "next/image";
+import connect from "../../../../backend/index";
+import News from "../../../../models/News";
+import Navbar from "../../components/Navbar";
 
-import connect from '../../../../backend/index'
-import News from '../../../../models/News'
-import Navbar from '../../components/Navbar';
-import Image from 'next/image';
+import { Raleway } from "next/font/google";
+const raleway = Raleway({ subsets: ["cyrillic-ext"] });
 
-// export async function getServerSideProps(params){
-//     const{id} = params.query;
-
-//     const res = await News.findById(id);
-//     // const data = await res.json()
-
-//     // console.log(data);
-
-//     return{
-//         props: {
-//             res,
-//         },
-//     };
-    
-// }
-
-
-// export default function BeritaId({param}){
-//     console.log(param);
-//     // const params = useParams();
-//     // console.log(params);
-//     return (
-//         <>
-//         <div key={param?._id}>
-//             <h2>
-//                 {param?.title}
-//             </h2>
-//             <p>
-//                 {param?.description}
-//             </p>
-//         </div>
-//         </>
-//     )
-// }
-// export const getsStaticspath =async()=>{
-//   const router = useRouter()
-//   const id = router.query.slug; 
-//   try {
-//     await connect()
-
-//   } catch (error) {
-    
-//   }
-// }
-
-export const getServerSideProps = async(context)=>{
-//   const router = useRouter(context)
+export const getServerSideProps = async (context) => {
   const id = context.query.slug;
-//   console.log(id);
-  // const id = router.query.slug;
-    try{
-        console.log("connecting");
-        await connect();
-        console.log("connected DB");
+  try {
+    console.log("connecting");
+    await connect();
+    console.log("connected DB");
 
-        console.log("fetching Document");
-        const get = await News.findById(id);
-        console.log(get);
-        console.log("fetched succesful");
+    console.log("fetching Document");
+    const get = await News.findById(id);
+    console.log(get);
+    console.log("fetched succesful");
 
-        return {
-            props: { 
-                get: JSON.parse(JSON.stringify(get)),
-            },
-        };
+    return {
+      props: {
+        get: JSON.parse(JSON.stringify(get)),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    };
+  }
+};
 
-    }catch(error){
-        console.log(error);
-        return{
-            notFound: true,
-        }
-    }
-}
- 
-export default function Page({get}) {
-//  console.log(get);  
+export default function Page({ get }) {
   return (
     <>
-    <Navbar/>
-      <div>
+      <Navbar />
+      <div className={`${raleway.className} p-[10%] pt-15`}>
+        <h1 className="font-bold text-xl ">{get?.title}</h1>
+        <p className="text-xs mb-5">Diposting pada {get?.createdAt}</p>
         <Image
-          src={'/'+get.image}
-          height={500}
-          width={500}
-          alt="foto Berita"
+          className="rounded-xl"
+          src={`/${get.image}`}
+          alt={get?.title}
+          width={5760}
+          height={3280}
         />
-          <h1>{get?.title}</h1>
-          <p>{get?.desc}</p>
+        <p className="mt-5">{get?.desc}</p>
       </div>
     </>
-    
-  )
+  );
 }
-
-// const getidnews = async()=>{
-//   try {
-//     await connect();
-//     const get = await News.findById({id})
-//     console.log(get);
-//   } catch (error) {
-      
-//   }
-// }
